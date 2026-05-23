110ceef8e94457bacc5c1d59145a133d2bec46f4
syahidanAS <syahidan139@gmail.com>
2026-05-23 08:54:47 +0700

    hotfix 1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+            $middleware->appendToGroup('web', [
+        \App\Http\Middleware\TrustProxies::class,
+    ]);
 
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
